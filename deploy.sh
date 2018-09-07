65a3c199f85d9f4cf9d9d7df1b8fe83d2cfc083a
buildqa <s1152yb68@contbay.com>
2018-09-07 00:46:31 -0400

    push and delete tag after release is deleted to completely remove a release entry on the github web page

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 
 # Example,
 # $ sh -x deploy.sh add
@@ -9,7 +9,11 @@
 
 action=""
 if [ "$1" == '' ]; then
-   echo "Must provide action as first argument <add,delete>"
+   echo "$0 <add,delete> [<release name>]"
+   echo "Must provide action as first argument <add,delete> followed by optional release name [<release name>]"
+   echo "The release name defaults to the current year-month-day without a timestamp, e.g., $(date '+%Y-%m-%d')"
+   echo "=> $0 add $(date '+%Y-%m-%d')"
+   echo "=> $0 delete $(date '+%Y-%m-%d')"
    exit 1
 elif [ "$1" == "add" ]; then
    action="add"
@@ -22,57 +26,44 @@ fi
 
 if [ "$2" == '' ]; then
   # default to current date for making/updating multiple builds on the same day
-  date=$(date '+%Y-%m-%d')
+  rel_name=$(date '+%Y-%m-%d')
 else
-  date=$1
+  rel_name=$2
 fi
 
-# test if release with $date tag exists
+# test if release with $rel_name tag exists
 release_exists="FALSE"
-./manage_assets.py buildqa tester1 "$date" test
+./manage_assets.py buildqa tester1 "$rel_name" test
 if [ $? == 0 ]; then
    release_exists="TRUE"
-   echo "Release $date already exists" 
+   echo "Release $rel_name already exists"
 fi
 
 if [ "$action" == "delete" ]; then
    if [ "$release_exists" == "TRUE" ]; then
-      # Run script to find and delete assets that were explicitly uploaded, but cannot
-      # delete the source archives that got created by default when ther release was first made.
-      # Apparently the API does not report the source archives as assets with ID numbers
-      # hence they cannot be found and deleted via unique ID's (unlike explcitily uploaded assets).
-      ./manage_assets.py buildqa tester1 "$date" delete all;
-   
-      # Mac:   hub release (via homebrew)
-      # Linux: github release
-      #
-      # This will technically delete the release on github and change the web page display
-      # so the tag does not look like a real release, however the default source archives:
-      # Source code (zip)
-      # Source code (tar.gz)
-      # will remain (see above comments).
-      #
-      # If you subsequently run this script again with the deletedd release/tag name, the git api will in fact
-      # report that it does not exist, and it will "resurrect" the release with the old source archives.
-      # Twisted and evil the API is with the dark side >;-O
-      #   
-      # Comments on stackoverflow seem to indicate source archives can only be deleted on the paid version of github ??
-      # https://stackoverflow.com/questions/45240336/how-to-use-github-release-api-to-make-a-release-without-source-code
-      hub release delete "$date"
-      # tag will not be found
-      # git tag --delete "$date"
+      # Run script to find and delete assets that were explicitly uploaded for the release.
+      # Then the release is deleted, and then the tag for the release is deleted.  The
+      # source archives created by default when ther release was first made are deleted
+      # when the tag is removed.
+      ./manage_assets.py buildqa tester1 "$rel_name" delete all;
+
+      # delete the release
+      hub release delete "$rel_name"
+
    elif [ "$release_exists" == "FALSE" ]; then
-      echo "Git API reports release $date does not exist so nothing to do for action = delete"
-   fi 
+      echo "Git API reports release $rel_name does not exist so nothing to do for action = delete"
+   fi
+   # always try to delete the tag if release does not exist
+   git push --delete origin $rel_name || true
 
 elif [ "$action" == "add" ]; then
 
    if [ "$release_exists" == "TRUE" ]; then
-      echo "Git API reports release $date already exists so nothing to do for action = add"
+      echo "Git API reports release $rel_name already exists so nothing to do for action = add"
    elif [ "$release_exists" == "FALSE" ]; then
       echo "Making new release";
-      # release name the same as tag name
-      hub release create -m "$date" "$date";
+      # create the release and tag
+      hub release create -m "$rel_name" "$rel_name";
    fi
 fi
 
